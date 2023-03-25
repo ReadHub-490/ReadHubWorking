76d0b6b2c27b12007219daacd4cd7afc5ce53a7f
Colton Scott <83851807+coltscott@users.noreply.github.com>
2023-03-25 15:24:22 -0400

    following page ui and back button functionality

---
+++ ReadHub/FollowingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FollowingViewController.swift
+//  ReadHub
+//
+//  Created by Colton Scott on 3/25/23.
+//
+
+import UIKit
+
+class FollowingViewController: UIViewController {
+
+}
+extension FollowingViewController {
+  @IBAction func cancelToFollowingViewController(_ segue: UIStoryboardSegue) {
+  }
+}
